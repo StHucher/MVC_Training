97a890c9f5f1194b6703025299bf68b3abce8b61
StHucher <stephane.hucher40@gmail.com>
2022-06-28 14:43:46 +0200

    Roads created

--- public/index.php
+++ public/index.php
@@ -4,3 +4,28 @@ require __DIR__.'/../app/controllers/MainController.php';
 
 $router = new AltoRouter();
 $router->setBasePath(dirname($_SERVER['SCRIPT_NAME']));
+
+
+$router->map('GET', '/', 'MainController::home');
+$router->map('GET', '/about', 'MainController::about');
+$router->map('GET', '/about/autor', 'MainController::aboutMe');
+
+$match = $router->match();
+
+if($match){
+
+  $match['target'] = $action;
+
+  $pieces = explode("::", $action);
+
+  $controllerName=$pieces[0];
+  $methodName=$pieces[1];
+
+  $controller = new $controllerName();
+  $controller->$methodName();
+
+}else{
+
+  $controller = new MainController();
+  $controller->err404();
+}
