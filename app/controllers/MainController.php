17a009d8121dae2515f3b7c6eb37964e6b328258
StHucher <stephane.hucher40@gmail.com>
2022-06-28 15:25:18 +0200

    Controller created with the first view

---
+++ app/controllers/MainController.php
@@ -0,0 +1,28 @@
+<?php
+
+class MainController {
+
+  public function home() {
+    $this->show('home');
+  }
+
+  public function about(){
+    $this->show('about',['title'=>'About']);
+  }
+
+  public function aboutMe(){
+    $this->show('autor', ['title'=> 'Who am I ?','day'=>date('l \t\h\e jS')]);
+  }
+
+  public function err404(){
+    $this->show('404');
+  }
+
+
+  private function show($viewName, $viewVars=[]){
+
+    $baseUrl = dirname($_SERVER['SCRIPT_NAME']);
+
+    require __DIR__."/../views/$viewName.tpl.php";
+  }
+}
