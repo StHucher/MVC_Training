9a68fed08adfac7d33b113b929e947648f5d7463
StHucher <stephane.hucher40@gmail.com>
2022-06-29 15:43:30 +0200

    README.md

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # MVC Training
 
 I create just a simple desing pattern MVC without Database.
+If you want to try this application,install Composer et make a  `composer.init`.
--- app/views/about.tpl.php
+++ app/views/about.tpl.php
@@ -1,4 +1,4 @@
 
       <h2> <?=$viewVars['title']?> </h2>
     
-      <p>I used Altorouter as router and Autoload to manage my Website. I created only one controller with 4 methods. </p>
+      <p>I used Altorouter as router to manage my roads. I created only one controller with 4 methods. </p>
